3f5f31e6d5df40cd63ffb93bea24963d0e65ba1b
Artem <artemusolcev4@gmail.com>
2022-12-03 21:35:07 +0700

    some variables renamed in favor of consistency

--- rinex_processor.py
+++ rinex_processor.py
@@ -36,9 +36,9 @@ class GpsNavMessageHeader:
 class GpsObservation:
     def __init__(self, Satellite_PRN_number: int, year: int, month: int, day: int, hour: int, minute: int,
                  second: float, SV_clock_bias: float, SV_clock_drift: float, SV_clock_drift_rate: float, IODE: float,
-                 Crs: float, Delta_n: float, M0: float, Cuc: float, e_Eccentricity: float, Cus: float, sqrt_A: float,
-                 Toe: float, Cic: float, OMEGA: float, CIS: float, i0: float, Crc: float, omega, OMEGA_DOT, IDOT,
-                 Codes_on_L2_channel, GPS_Week, L2_P, SV_accuracy, SV_health, TGD, IODS, Transmission_time_of_message,
+                 C_rs: float, Delta_n: float, M0: float, C_uc: float, e_Eccentricity: float, C_us: float, sqrt_A: float,
+                 T_oe: float, C_ic: float, OMEGA0: float, C_is: float, i0: float, C_rc: float, omega, OMEGA_DOT, IDOT,
+                 Codes_on_L2_channel, GPS_Week, L2_P, SV_accuracy, SV_health, TGD, IODS, t_tm,
                  Fit_interval
                  ):
         # === OBS. RECORD: PRN / EPOCH / SV CLK ===
@@ -49,22 +49,22 @@ class GpsObservation:
         self.SV_clock_drift_rate = SV_clock_drift_rate
         # === OBS. RECORD: BROADCAST ORBIT - 1 ===
         self.IODE = IODE  # Issue of Data, Ephemeris
-        self.Crs = Crs
+        self.C_rs = C_rs
         self.Delta_n = Delta_n
         self.M0 = M0
         # === OBS. RECORD: BROADCAST ORBIT - 2 ===
-        self.Cuc = Cuc
+        self.C_uc = C_uc
         self.e_Eccentricity = e_Eccentricity
-        self.Cus = Cus
+        self.C_us = C_us
         self.sqrt_A = sqrt_A
         # === OBS. RECORD: BROADCAST ORBIT - 3 ===
-        self.Toe = Toe
-        self.Cic = Cic
-        self.OMEGA = OMEGA
-        self.CIS = CIS
+        self.T_oe = T_oe
+        self.C_ic = C_ic
+        self.OMEGA0 = OMEGA0
+        self.C_is = C_is
         # === OBS. RECORD: BROADCAST ORBIT - 4 ===
         self.i0 = i0
-        self.Crc = Crc
+        self.C_rc = C_rc
         self.omega = omega
         self.OMEGA_DOT = OMEGA_DOT
         # === OBS. RECORD: BROADCAST ORBIT - 5 ===
@@ -78,17 +78,17 @@ class GpsObservation:
         self.TGD = TGD
         self.IODS = IODS
         # === OBS. RECORD: BROADCAST ORBIT - 7 ===
-        self.Transmission_time_of_message = Transmission_time_of_message
+        self.t_tm = t_tm
         self.Fit_interval = Fit_interval
 
     def get_list(self) -> list:
         out_list = [self.Satellite_PRN_number, self.Epoch.year, self.Epoch.month, self.Epoch.day, self.Epoch.hour,
                     self.Epoch.minute, self.Epoch.second, self.SV_clock_bias, self.SV_clock_drift,
                     self.SV_clock_drift_rate,
-                    self.IODE, self.Crs, self.Delta_n, self.M0, self.Cuc, self.e_Eccentricity, self.Cus, self.sqrt_A,
-                    self.Toe, self.Cic, self.OMEGA, self.CIS, self.i0, self.Crc, self.omega, self.OMEGA_DOT, self.IDOT,
+                    self.IODE, self.C_rs, self.Delta_n, self.M0, self.C_uc, self.e_Eccentricity, self.C_us, self.sqrt_A,
+                    self.T_oe, self.C_ic, self.OMEGA0, self.C_is, self.i0, self.C_rc, self.omega, self.OMEGA_DOT, self.IDOT,
                     self.Codes_on_L2_channel, self.GPS_Week, self.L2_P, self.SV_accuracy, self.SV_health,
-                    self.TGD, self.IODS, self.Transmission_time_of_message, self.Fit_interval]
+                    self.TGD, self.IODS, self.t_tm, self.Fit_interval]
         return out_list
     """
     def calculate(self):
@@ -192,19 +192,19 @@ class GpsNavigationMessageFile:
 
             # === OBS. RECORD: BROADCAST ORBIT - 1 ===
             bo1 = self.read_line(self.data[k * 8 + 2 + self.header_end_line])
-            IODE, Crs, Delta_n, M0 = bo1[0], bo1[1], bo1[2], bo1[3]
+            IODE, C_rs, Delta_n, M0 = bo1[0], bo1[1], bo1[2], bo1[3]
 
             # === OBS. RECORD: BROADCAST ORBIT - 2 ===
             bo2 = self.read_line(self.data[k * 8 + 3 + self.header_end_line])
-            Cuc, e_Eccentricity, Cus, sqrt_A = bo2[0], bo2[1], bo2[2], bo2[3]
+            C_uc, e_Eccentricity, C_us, sqrt_A = bo2[0], bo2[1], bo2[2], bo2[3]
 
             # === OBS. RECORD: BROADCAST ORBIT - 3 ===
             bo3 = self.read_line(self.data[k * 8 + 4 + self.header_end_line])
-            Toe, Cic, OMEGA, CIS = bo3[0], bo3[1], bo3[2], bo3[3]
+            T_oe, C_ic, OMEGA0, C_is = bo3[0], bo3[1], bo3[2], bo3[3]
 
             # === OBS. RECORD: BROADCAST ORBIT - 4 ===
             bo4 = self.read_line(self.data[k * 8 + 5 + self.header_end_line])
-            i0, Crc, omega, OMEGA_DOT = bo4[0], bo4[1], bo4[2], bo4[3]
+            i0, C_rc, omega, OMEGA_DOT = bo4[0], bo4[1], bo4[2], bo4[3]
 
             # === OBS. RECORD: BROADCAST ORBIT - 5 ===
             bo5 = self.read_line(self.data[k * 8 + 6 + self.header_end_line])
@@ -216,14 +216,14 @@ class GpsNavigationMessageFile:
 
             # === OBS. RECORD: BROADCAST ORBIT - 7 ===
             bo7 = self.read_line(self.data[k * 8 + 8 + self.header_end_line])
-            Transmission_time_of_message, Fit_interval = bo7[0], bo7[1]
+            t_tm, Fit_interval = bo7[0], bo7[1]
 
             self.observations.append(GpsObservation(Satellite_PRN_number, year, month, day, hour, minute,
                                                     second, SV_clock_bias, SV_clock_drift, SV_clock_drift_rate, IODE,
-                                                    Crs, Delta_n, M0, Cuc, e_Eccentricity, Cus, sqrt_A,
-                                                    Toe, Cic, OMEGA, CIS, i0, Crc, omega, OMEGA_DOT, IDOT,
+                                                    C_rs, Delta_n, M0, C_uc, e_Eccentricity, C_us, sqrt_A,
+                                                    T_oe, C_ic, OMEGA0, C_is, i0, C_rc, omega, OMEGA_DOT, IDOT,
                                                     Codes_on_L2_channel, GPS_Week, L2_P, SV_accuracy, SV_health,
-                                                    TGD, IODS, Transmission_time_of_message, Fit_interval))
+                                                    TGD, IODS, t_tm, Fit_interval))
 
     def create_csv_sheet(self):
         try:
@@ -234,9 +234,9 @@ class GpsNavigationMessageFile:
 
         writer = csv.writer(file)
         header = 'Satellite_PRN_number, year, month, day, hour, minute, second, SV_clock_bias, SV_clock_drift, ' \
-                 'SV_clock_drift_rate, IODE, Crs, Delta_n, M0, Cuc, e_Eccentricity, Cus, sqrt_A, Toe, Cic, OMEGA, ' \
-                 'CIS, i0, Crc, omega, OMEGA_DOT, IDOT, Codes_on_L2_channel, GPS_Week, L2_P, SV_accuracy, SV_health, ' \
-                 'TGD, IODS, Transmission_time_of_message, Fit_interval'.replace(' ', '').split(',')
+                 'SV_clock_drift_rate, IODE, C_rs, Delta_n, M0, C_uc, e_Eccentricity, C_us, sqrt_A, T_oe, C_ic, ' \
+                 'OMEGA0, C_is, i0, C_rc, omega, OMEGA_DOT, IDOT, Codes_on_L2_channel, GPS_Week, L2_P, SV_accuracy, ' \
+                 'SV_health, TGD, IODS, t_tm, Fit_interval'.replace(' ', '').split(',')
         writer.writerow(header)
         for i in range(len(self.observations)):
             data = self.observations[i].get_list()
